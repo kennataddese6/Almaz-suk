2a4791aed103ef81fd9d9b6766b452506ab14150
kennataddese6 <kennataddese6@gmail.com>
2025-02-18 19:36:42 +0300

    add a hero text

---
+++ app/components/Hero.tsx
@@ -0,0 +1,22 @@
+const Hero = () => {
+  return (
+    <div className=" w-full h-screen flex items-center px-16 text-black">
+      <div>
+        <h1 className="text-5xl my-3 text-black font-semibold">
+          Discover the Finest Products <br /> in Ethiopia
+        </h1>
+        <p>
+          We are very reliable buisness service the most up to data products to
+          our customers with the <br />
+          greatest time etc
+        </p>
+        <button className="px-12 py-2 bg-blue-500 my-3 text-white">
+          Shop Now
+        </button>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Hero
