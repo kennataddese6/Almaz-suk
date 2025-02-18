3dc9e04f13d220e1ad696c4a790b30e8664e8e08
kennataddese6 <kennataddese6@gmail.com>
2025-02-18 19:54:26 +0300

    add hero and add small responsiveness

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,21 +2,21 @@ import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div className=" w-full h-screen flex items-center px-16 text-black">
-      <div>
-        <h1 className="text-5xl my-3 text-black font-semibold">
+    <div className="w-full h-screen flex items-center px-16 text-black flex-wrap pt-28 md:pt-0">
+      <div className="w-full md:w-1/2">
+        <h1 className="text-5xl my-3 text-black font-semibold text-center md:text-start">
           Discover the Finest Products <br /> in Ethiopia
         </h1>
-        <p>
+        <p className="text-center md:text-start">
           We are very reliable buisness service the most up to data products to
           our customers with the <br />
           greatest time etc
         </p>
-        <button className="px-12 py-2 bg-blue-500 my-3 text-white">
+        <button className="px-12 py-2 bg-blue-500 my-3 text-white mx-auto block md:mx-0">
           Shop Now
         </button>
       </div>
-      <div className="w-1/2 flex justify-center">
+      <div className="w-full md:w-1/2 flex justify-center">
         <Image src={"/bag.jpg"} alt="Hero Image" width={600} height={600} />
       </div>
     </div>
