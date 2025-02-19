d3cf947d9a54823e9ecec1270a12117ffb2b070d
kennataddese6 <kennataddese6@gmail.com>
2025-02-19 14:59:07 +0300

    add text

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
           Discover the Finest Products <br /> in Ethiopia
         </h1>
         <p className="text-center md:text-start">
-          Discover Ethiopia's finest products, handpicked for quality and
+          Discover Ethiopias finest products, handpicked for quality and
           craftsmanship. Elevate your lifestyle with our exclusive selection.
         </p>
         <button className="px-12 py-2 bg-blue-500 my-3 text-white mx-auto block md:mx-0">
